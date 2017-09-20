7fd33651113394ab4b8965cddae0353738f53741
Kristiyan Tsaklev <cuki@cukii.tk>
2017-09-20 15:13:04 +0300

    Fix task upload

--- dmoj_uploader/upload_task.sh
+++ dmoj_uploader/upload_task.sh
@@ -86,13 +86,14 @@ add_problem() {
 		--form "is_public=on" \
 		--form "date_0=2017-08-18" \
 		--form "date_1=12:00:00" \
+		--form "authors=1" \
 		--form "description=$description" \
 		--form "license=" \
 		--form "og_image=" \
 		--form "summary=" \
-		--form "types=3" \
-		--form "types=5" \
-		--form "group=7" \
+		--form "types=1" \
+		--form "types=2" \
+		--form "group=1" \
 		--form "points=$points" \
 		--form "partial=on" \
 		--form "time_limit=$time_limit" \
@@ -101,7 +102,36 @@ add_problem() {
 		--form "allowed_languages=2" \
 		--form "allowed_languages=3" \
 		--form "allowed_languages=4" \
+		--form "allowed_languages=5" \
+		--form "allowed_languages=6" \
+		--form "allowed_languages=7" \
+		--form "allowed_languages=8" \
+		--form "allowed_languages_all=on" \
 		--form "change_message=" \
+		--form "language_limits-TOTAL_FORMS=3" \
+		--form "language_limits-INITIAL_FORMS=0" \
+		--form "language_limits-MIN_NUM_FORMS=0" \
+		--form "language_limits-MAX_NUM_FORMS=1000" \
+		--form "language_limits-0-id=" \
+		--form "language_limits-0-problem=" \
+		--form "language_limits-0-language=" \
+		--form "language_limits-0-time_limit=" \
+		--form "language_limits-0-memory_limit=" \
+		--form "language_limits-1-id=" \
+		--form "language_limits-1-problem=" \
+		--form "language_limits-1-language=" \
+		--form "language_limits-1-time_limit=" \
+		--form "language_limits-1-memory_limit=" \
+		--form "language_limits-2-id=" \
+		--form "language_limits-2-problem=" \
+		--form "language_limits-2-language=" \
+		--form "language_limits-2-time_limit=" \
+		--form "language_limits-2-memory_limit=" \
+		--form "language_limits-__prefix__-id=" \
+		--form "language_limits-__prefix__-problem=" \
+		--form "language_limits-__prefix__-language=" \
+		--form "language_limits-__prefix__-time_limit=" \
+		--form "language_limits-__prefix__-memory_limit=" \
 		--form "problemclarification_set-TOTAL_FORMS=0" \
 		--form "problemclarification_set-INITIAL_FORMS=0" \
 		--form "problemclarification_set-MIN_NUM_FORMS=0" \
@@ -109,15 +139,15 @@ add_problem() {
 		--form "problemclarification_set-__prefix__-description=" \
 		--form "problemclarification_set-__prefix__-id=" \
 		--form "problemclarification_set-__prefix__-problem=" \
-		--form "language_limits-TOTAL_FORMS=0" \
-		--form "language_limits-INITIAL_FORMS=0" \
-		--form "language_limits-MIN_NUM_FORMS=0" \
-		--form "language_limits-MAX_NUM_FORMS=1000" \
-		--form "language_limits-__prefix__-id=" \
-		--form "language_limits-__prefix__-problem=" \
-		--form "language_limits-__prefix__-language=" \
-		--form "language_limits-__prefix__-time_limit=" \
-		--form "language_limits-__prefix__-memory_limit=" \
+		--form "solution-TOTAL_FORMS=0" \
+		--form "solution-INITIAL_FORMS=0" \
+		--form "solution-MIN_NUM_FORMS=0" \
+		--form "solution-MAX_NUM_FORMS=1" \
+		--form "solution-__prefix__-publish_on_0=" \
+		--form "solution-__prefix__-publish_on_1=" \
+		--form "solution-__prefix__-content=" \
+		--form "solution-__prefix__-id=" \
+		--form "solution-__prefix__-problem=" \
 		--form "translations-TOTAL_FORMS=0" \
 		--form "translations-INITIAL_FORMS=0" \
 		--form "translations-MIN_NUM_FORMS=0" \
@@ -131,3 +161,6 @@ add_problem() {
 }
 
 [[ -f "$COOKIE_JAR" ]] || auth
+
+add_problem autotest "Auto test" 'Multiline
+кирилица'
