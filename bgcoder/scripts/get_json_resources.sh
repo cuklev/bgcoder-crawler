1e31c8c83e3b57f74bfc3d6b62ec73e4abebf8c3
Kristiyan Tsaklev <cuki@cukii.tk>
2017-10-05 20:04:58 +0300

    Readability

--- bgcoder/scripts/get_json_resources.sh
+++ bgcoder/scripts/get_json_resources.sh
@@ -2,4 +2,9 @@
 
 PROBLEM_ID="$1"
 
-curl -s "$OJS_URL/Administration/Resources/GetAll/$PROBLEM_ID" -b "$COOKIE_JAR" --compressed --data 'sort=OrderBy-asc&page=1&pageSize=50&group=&filter='
+curl -s "$OJS_URL/Administration/Resources/GetAll/$PROBLEM_ID" -b "$COOKIE_JAR" --compressed \
+	--data 'sort=OrderBy-asc' \
+	--data 'page=1' \
+	--data 'pageSize=50' \
+	--data 'group=' \
+	--data 'filter='
