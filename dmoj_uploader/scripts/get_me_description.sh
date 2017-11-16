1df3d277d8bacfcd17fc574cd5caf1aa26f8323f
Kristiyan Tsaklev <cuki@cukii.tk>
2017-11-16 17:06:58 +0200

    Support doc{x,} files

--- dmoj_uploader/scripts/get_me_description.sh
+++ dmoj_uploader/scripts/get_me_description.sh
@@ -2,13 +2,20 @@
 
 PROBLEM_DIR="$1"
 cd "$PROBLEM_DIR"
-
 cd resources
 
 md_file() {
 	local file="$(find -iname \*.md | head -n1)"
-	if [[ "$md_file" != "" ]]; then
-		cat "$md_file"
+	if [[ "$file" != "" ]]; then
+		cat "$file"
+		exit
+	fi
+}
+
+html_file() {
+	local file="$(find -iname \*.html | head -n1)"
+	if [[ "$file" != "" ]]; then
+		cat "$file"
 		exit
 	fi
 }
@@ -22,7 +29,36 @@ link_file() {
 	fi
 }
 
-md_file
-link_file
+odt_file() {
+	local file="$(find -iname \*.odt | head -n1)"
+	if [[ "$file" != "" ]]; then
+		pandoc "$file" -t markdown_github -o-
+		exit
+	fi
+}
 
-exit 0
+doc_file() {
+	local file="$(find -iname \*.doc | head -n1)"
+	if [[ "$file" != "" ]]; then
+		lowriter --convert-to odt "$file" &> /dev/null
+		odt_file
+		exit
+	fi
+}
+
+docx_file() {
+	local file="$(find -iname \*.docx | head -n1)"
+	if [[ "$file" != "" ]]; then
+		lowriter --convert-to odt "$file" &> /dev/null
+		odt_file
+		exit
+	fi
+}
+
+md_file
+html_file
+link_file
+docx_file
+doc_file
+
+exit 1
